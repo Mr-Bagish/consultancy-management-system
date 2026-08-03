73e24983d2db729e39e69295768e0d63848e9be4
Bagish <bagishgautam4@gmail.com>
2026-08-03 16:10:28 +0545

    removed old unncessary code

--- consultancy/bookings/views.py
+++ consultancy/bookings/views.py
@@ -1,17 +1,3 @@
-from django.http import JsonResponse
-
-def all_bookings(request):
-    return JsonResponse({'message': 'Week 2 - coming soon'})
-
-def update_status(request, pk):
-    return JsonResponse({'message': 'Week 2 - coming soon'})
-
-def assign_consultant(request, pk):
-    return JsonResponse({'message': 'Week 3 - coming soon'})
-
-def analytics(request):
-    return JsonResponse({'message': 'Week 3 - coming soon'})
-
 from rest_framework.generics import CreateAPIView
 from .models import Booking
 from .serializers import BookingSerializer
